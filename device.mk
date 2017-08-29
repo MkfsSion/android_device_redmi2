e81c99dbc3998cd96eaf41c74446f650d4fc9b4f
ssawrav <s.sawrav@gmail.com>
2017-08-29 14:50:18 +0530

    Revert "Don't Build camera.msm8916"
    This reverts commit d7e236067cc1556d3ab934966949313d1a217e51.

--- device.mk
+++ device.mk
@@ -244,6 +244,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl \
+    camera.msm8916 \
     Snap \
     libmm-qcamera \
     libshim_camera \
