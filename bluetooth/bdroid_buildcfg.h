d22e21aa3093d38054e6dec6d7f34aab21bfb16c
ssawrav <s.sawrav@gmail.com>
2017-09-04 22:53:49 +0530

    Update Bdroid config

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -23,5 +23,7 @@
 #define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED  FALSE
-#define BT_CLEAN_TURN_ON_DISABLED TRUE
+// skips conn update at conn completion
+#define BTA_BLE_SKIP_CONN_UPD  FALSE
+#define BLE_PERIPHERAL_ADV_NAME  TRUE
 #endif
